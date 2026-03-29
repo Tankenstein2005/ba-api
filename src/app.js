6729ef1ac461afa996bf2e032a179c78dec2216a
Tankenstein2005 <abhinav.vashisht.7@gmail.com>
2026-03-29 14:53:08 +0530

    Few more bug fixes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,20 @@ import express from "express";
 import { env } from "./config/env.js";
 import router from "./routes/index.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import { httpError } from "./utils/httpError.js";
 
 const app = express();
+const defaultAllowedOrigins = [
+  "http://localhost:443",
+  "http://localhost:5173",
+  "https://ba-workcenter.vercel.app",
+  "https://ba-workcenter-*.vercel.app",
+  "https://ba-workcenter-git-*.vercel.app"
+];
+const allowedOrigins = [...new Set([...defaultAllowedOrigins, ...env.clientUrls])];
 
 function isAllowedOrigin(origin) {
-  return env.clientUrls.some((allowedOrigin) => {
+  return allowedOrigins.some((allowedOrigin) => {
     if (allowedOrigin.includes("*")) {
       const pattern = new RegExp(
         `^${allowedOrigin
@@ -24,18 +33,22 @@ function isAllowedOrigin(origin) {
   });
 }
 
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin || isAllowedOrigin(origin)) {
-        callback(null, true);
-        return;
-      }
-
-      callback(new Error(`Origin ${origin} is not allowed by CORS`));
+const corsOptions = {
+  origin(origin, callback) {
+    if (!origin || isAllowedOrigin(origin)) {
+      callback(null, true);
+      return;
     }
-  })
-);
+
+    callback(
+      httpError(403, `Origin ${origin} is not allowed by CORS`)
+    );
+  },
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use("/api", router);
 app.use(errorHandler);
